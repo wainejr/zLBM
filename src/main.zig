4a85e8e3661d8b6cc316687328290ca3d1710de4
Waine Oliveira Jr <waine@aerosim.io>
2024-07-31 15:11:58 -0300

    Started adding zig tracy

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,12 @@ const std = @import("std");
 const lbm = @import("lbm.zig");
 const vtk = @import("vtk.zig");
 const defs = @import("defines.zig");
+const tracy = @import("tracy");
 
 pub fn main() !void {
+    tracy.setThreadName("Other");
+    defer tracy.message("Graceful other thread exit");
+
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
@@ -12,9 +16,10 @@ pub fn main() !void {
     const lbm_arrays = try lbm.allocate_arrs(&allocator);
     lbm_arrays.initialize();
 
-    try lbm_arrays.export_arrays(allocator, 0);
+    // try lbm_arrays.export_arrays(allocator, 0);
     var timer = try std.time.Timer.start();
 
+    const zone = tracy.initZone(@src(), .{ .name = "Simulation" });
     for (1..(defs.n_steps + 1)) |time_step| {
         lbm.run_time_step(lbm_arrays, @intCast(time_step));
         if (time_step % defs.freq_export == 0) {
@@ -22,6 +27,8 @@ pub fn main() !void {
             std.debug.print("Exported arrays in time step {}\n", .{time_step});
         }
     }
+    zone.deinit();
+
     const time_passed_nano: f32 = @floatFromInt(timer.lap());
     const time_passed_sec: f32 = time_passed_nano / 1e9;
 
