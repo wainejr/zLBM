bb3dcbde87bca5647e9650991ac031b84dbec28f
Waine Oliveira Jr <waine@aerosim.io>
2024-07-30 22:49:51 -0300

    Added library to comptime

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 const testing = std.testing;
 
+comptime {
+    _ = @import("lbm.zig");
+    _ = @import("vtk.zig");
+}
+
 export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
